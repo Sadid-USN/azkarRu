71ef44befc31f4dbc3dc71eff8320f0b5402375c
Sadid-USN <ulamuyaman@gmail.com>
2023-10-09 00:24:24 +0600

    fixed ios louncher

--- lib/core/addbunner_helper.dart
+++ lib/core/addbunner_helper.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class BannerAdHelper {
   static final BannerAdHelper _instance = BannerAdHelper._internal();
   late BannerAd bannerAd;
-  final String adUnitId = "ca-app-pub-7613540986721565/1251576374";
+  final String adUnitIdAndroid = "ca-app-pub-7613540986721565/1251576374";
+  final String adUnitIdIOS = "ca-app-pub-7613540986721565/5492028111";
   bool isBannerAd = false;
 
   factory BannerAdHelper() {
@@ -15,7 +18,7 @@ class BannerAdHelper {
   void initializeAdMob({required void Function(Ad)? onAdLoaded}) {
     bannerAd = BannerAd(
       size: AdSize.banner,
-      adUnitId: adUnitId,
+      adUnitId: Platform.isAndroid ? adUnitIdAndroid : adUnitIdIOS,
       listener: BannerAdListener(
         onAdLoaded: onAdLoaded,
         onAdFailedToLoad: (ad, error) {
